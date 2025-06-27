388f150d972add4cf202bf402110ee051753c11d
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-06-27 23:46:10 +0700

    Update urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("{{ project_name }}-admin/", admin.site.urls),
-    path("-/", include("django_alive.urls")),
+    path("admin/", admin.site.urls),
 ]
