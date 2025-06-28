fc1fadea6e62790cd98e662e0d92f260530aef0e
Дмитрий <154452992+sh1ndoo@users.noreply.github.com>
2025-06-28 23:30:41 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main() -> None:
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
